c93611c2e1610b13281868a726de543779f405ad
GoodStuff11 <34872749+GoodStuff11@users.noreply.github.com>
2019-02-28 10:54:00 -0500

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,8 +1,34 @@
 #include "element.cpp"
+#include <string>
+using namespace std;
 class list{
+private:
+	template <class T>
+	void addItem(T x){
+		if (*first == NULL){
+			element<T> newElement = new element<T>(x);
+			*first = &newElement;
+		}else if (*last == NULL){
+			element<T> newElement = new element<T>(x);
+			*last = &newElement;
+			first->pointer = &last;
+		}else{
+			element<T> newElement = new element<T>(x);
+			(*last)->pointer = &newElement;
+			*last = &newElement;
+		}
+	}
 public:
-	element *start;
+	element *first;
 	element *last;
 	list();
-	addString()
+	void addString(string str){
+		addItem<string>(str);
+	}
+	void addDouble(double num){
+		addItem<double>(num);
+	}
+	void addInt(int num){
+		addItem<int>(num);
+	}
 };
