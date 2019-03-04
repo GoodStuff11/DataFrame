933b7d9157cf0b0dd2609d9bfb69c10ededbd831
GoodStuff11 <34872749+GoodStuff11@users.noreply.github.com>
2019-03-04 12:27:55 -0500

    todos

--- list.cpp
+++ list.cpp
@@ -15,12 +15,13 @@ private:
     element <t>*last;
 public:
     ~list(){
-        //modify
+        //TODO: modify
     }
     int size(){
         return length;
     }
     void push_back(t val){
+        //TODO: remove last or add functionality
         if (first == NULL){
             first = new element <t>{val};
         }else if(last == NULL){
@@ -38,6 +39,25 @@ public:
         for (int i=0;i<index;i++){
             temp = temp->ptr;
         }
-        return temp->val; //wont work
+        return temp->val;
+    }
+    void erase(int index){
+        //TODO: make this work with first and last value
+        element <t> *temp = first;
+        for (int i=0;i<index-1;i++){
+            temp = temp->ptr;
+        }
+        //connect the one before to the one after
+        temp->ptr = temp->ptr->ptr->ptr;
+        delete temp->ptr->ptr;
+        length--;
     }
 };
+int main(){
+    list <int> *L = new list<int>();
+    for (int i=0;i<10;i++)
+        L->push_back(i);
+    L->erase(5);
+    //TODO: make iterator
+    for (int i=0;i<9;i++)
+}
