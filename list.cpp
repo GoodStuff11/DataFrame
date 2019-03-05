14b75d259a0babf673009b4858250f2c6913f511
GoodStuff11 <34872749+GoodStuff11@users.noreply.github.com>
2019-03-05 00:20:10 -0500

    fixed bugs

--- list.cpp
+++ list.cpp
@@ -12,16 +12,23 @@ class list{
 private:
     int length;
     element <t>*first;
-    element <t>*last;
+    element <t>*last; 
 public:
     ~list(){
-        //TODO: modify
+        //go from one pointer to the next deleting each one
+        element <t>*toDelete = first;
+        for (int i=0;i<this->size();i++){
+            element <t>*temp = toDelete->ptr;
+            delete toDelete;
+            toDelete = temp;
+        }
+        delete toDelete;
+        delete length;
     }
     int size(){
         return length;
     }
     void push_back(t val){
-        //TODO: remove last or add functionality
         if (first == NULL){
             first = new element <t>{val};
         }else if(last == NULL){
@@ -44,20 +51,29 @@ public:
     void erase(int index){
         //TODO: make this work with first and last value
         element <t> *temp = first;
-        for (int i=0;i<index-1;i++){
+        for (int i=0;i<index-1;i++)
             temp = temp->ptr;
+        
+        if (index == 0){
+            first = temp->ptr;
+            delete temp;
+        }else if (index == this->size() - 1){
+            delete &temp->ptr;
+            temp->ptr = NULL;
+            last = temp;
+        }else{
+            //connect the one before to the one after
+            temp->ptr = temp->ptr->ptr;
+            delete &temp->ptr->ptr;
         }
-        //connect the one before to the one after
-        temp->ptr = temp->ptr->ptr->ptr;
-        delete temp->ptr->ptr;
         length--;
     }
+    
 };
 int main(){
     list <int> *L = new list<int>();
     for (int i=0;i<10;i++)
         L->push_back(i);
-    L->erase(5);
-    //TODO: make iterator
-    for (int i=0;i<9;i++)
+    delete L;
+
 }
