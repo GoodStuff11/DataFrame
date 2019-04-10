e63fb7d7bde682794cb8456dfcf5b60d460fdce7
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-10 10:51:58 -0400

    Update network.h

--- network.h
+++ network.h
@@ -1,24 +1,34 @@
-#ifndef thing
-#define thing
+#ifndef SRC_PERSON_H_
+#define SRC_PERSON_H_
 
+#include <string>
 #include <iostream>
+#include <fstream>
+#include "list.h"
+#include "person.h"
 
-struct Person {
-	int id;
-	char firstName[10];
-	char surname[10];
-	int age;
-	int height;
+using namespace std;
 
-	int numContacts = 0;
-	//array of pointers which point to an instance stores in
-	//Network's people list
-	Person **contacts;
-	Person(char *first,char *last,int age,int height);
-	void addContact(Person *p);
-	void removeContact(Person *p);
-	bool operator==(Person *other);
 
-	virtual ~Person();
+class Network {
+private:
+	list< Person*> *contactPath( Person *p0,  Person *pf,
+			list< Person*> *contacted, list< Person*> contactOrder,
+			list<int> turnList, int turn);
+
+public:
+	list<Person> *people;
+	list <int> relationships;
+	Network();
+	int size();
+	virtual ~Network();
+	void addPerson(Person p);
+	void removePerson(Person *p);
+	void addRelationship(Person *p1, Person *p2);
+	void contactPath( Person *p0,  Person *pf);
+	bool in( Person *val,  list< Person*> *personList, int turn,list<int> *turnList);
+	Person* getPerson(string firstName, string surname);
+	void write(char* filename);
 };
-#endif
+
+#endif /* SRC_PERSON_H_ */
