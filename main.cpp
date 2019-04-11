71895ab45155e8b9233c417de3edd8f55b4d5c10
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-11 10:42:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include "network.h"
 
 using namespace std;
-struct A{
-	int a;
-};
+
 int main(){
 
     Network *net = new Network();
@@ -20,15 +18,21 @@ int main(){
     Person *Bo = net->getPerson("Bo","Izeer2");
     Person *Bo3 = net->getPerson("Bo","Izeer3");
 
+
     net->addRelationship(Mike,Joe);
     net->addRelationship(Larry,Joe);
     net->addRelationship(Bo,Joe);
     net->addRelationship(Bo,Bo3);
 
-    net->contactPath(Mike,Bo3);
+    net->printNetwork();
+    net->write("s:/data/networkdata.bin");
+
     delete net;
 
+    Network *NEWnet = new Network();
+    NEWnet->read("s:/data/networkdata.bin");
+    NEWnet->printNetwork();
+
+    delete NEWnet;
 
 }
-
-
