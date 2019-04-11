dd68d59ceea79b87eed240d51a271b91a516e198
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-11 10:42:35 -0400

    Update network.h

--- network.h
+++ network.h
@@ -9,26 +9,30 @@
 
 using namespace std;
 
-
 class Network {
 private:
-	list< Person*> *contactPath( Person *p0,  Person *pf,
-			list< Person*> *contacted, list< Person*> contactOrder,
-			list<int> turnList, int turn);
-
+	list<Person*> *contactPath(Person *p0, Person *pf, list<Person*> *contacted,
+			list<Person*> contactOrder, list<int> turnList, int turn);
+	void addRelationship(int id1, int id2); //a user wouldn't need this
 public:
 	list<Person> *people;
-	list <int> relationships;
-	Network();
+	list<int> *relationships;
 	int size();
+
+	Network();
 	virtual ~Network();
+
 	void addPerson(Person p);
 	void removePerson(Person *p);
+
 	void addRelationship(Person *p1, Person *p2);
-	void contactPath( Person *p0,  Person *pf);
-	bool in( Person *val,  list< Person*> *personList, int turn,list<int> *turnList);
+
+	void contactPath(Person *p0, Person *pf);
+	bool in(Person *val, list<Person*> *personList, int turn, list<int> *turnList);
 	Person* getPerson(string firstName, string surname);
 	void write(char* filename);
+	void read(char* filename);
+	void printNetwork();
 };
 
 #endif /* SRC_PERSON_H_ */
